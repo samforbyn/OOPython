b91dee01659094c2b14c06081a7b5766eca165bd
Sam Forbyn <samforbyn@gmail.com>
2022-05-06 08:07:52 -0600

    finished OO TV w/ test code, fixed typos

--- tv_remote_simulations/OO_TV_with_Test_Code.py
+++ tv_remote_simulations/OO_TV_with_Test_Code.py
@@ -65,3 +65,33 @@ class TV():
                 print(f"Volume is: {self.volume}")
         else:
             print("TV is: OFF")
+
+#Main code
+oTV = TV() # create TV object
+
+#Turn the TV on and show the status
+oTV.power()
+oTV.showInfo()
+
+#Change the channel up twice, raise the volume twice, show status
+oTV.channelUp()
+oTV.channelUp()
+oTV.volumeUp()
+oTV.volumeUp()
+oTV.showInfo()
+
+#Turn the TV off, show status, turn the TV on, show status
+oTV.power()
+oTV.showInfo()
+oTV.power()
+oTV.showInfo()
+
+#Lower the volume, mute the sound, show status
+oTV.volumeDown()
+oTV.mute()
+oTV.showInfo()
+
+#Change the channel to 11, mute the sound, show status
+oTV.setChannel(11)
+oTV.mute()
+oTV.showInfo()
